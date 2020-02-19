35ef33da0b70665ed2faa89bf19cf9ea1a20f560
Alfredwooden <coomcs@gmail.com>
2020-02-19 15:50:48 -0300

    Section 3: Class 42

--- second-app/src/CommentDetail.js
+++ second-app/src/CommentDetail.js
@@ -1,57 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import faker from "faker";
 
-const CommentDetail = () => {
+const CommentDetail = props => {
   return (
     <div className="comment">
-    <a href="/" className="avatar">
-      <img alt="avatar" src={faker.image.avatar()} />
-    </a>
-
-    <div className="content">
-      <a href="/" className="author">
-        Sam
+      <a href="/" className="avatar">
+        <img alt="avatar" src={props.avatarImage} />
       </a>
-      <div className="metadata">
-        <span className="date">Today at 6:00PM</span>
+
+      <div className="content">
+        <a href="/" className="author">
+          {props.author}
+        </a>
+
+        <div className="metadata">
+          <span className="date">{props.timeAgo}</span>
+        </div>
+
+        <div className="text">{props.commentText}</div>
       </div>
-      <div className="text">Nice blog post!</div>
     </div>
-  </div>
-
-  <div className="comment">
-    <a href="/" className="avatar">
-      <img alt="avatar" src={faker.image.avatar()} />
-    </a>
-
-    <div className="content">
-      <a href="/" className="author">
-        Sam
-      </a>
-      <div className="metadata">
-        <span className="date">Today at 6:00PM</span>
-      </div>
-      <div className="text">Nice blog post!</div>
-    </div>
-  </div>
-
-  <div className="comment">
-    <a href="/" className="avatar">
-      <img alt="avatar" src={faker.image.avatar()} />
-    </a>
-
-    <div className="content">
-      <a href="/" className="author">
-        Sam
-      </a>
-      <div className="metadata">
-        <span className="date">Today at 6:00PM</span>
-      </div>
-      <div className="text">Nice blog post!</div>
-    </div>
-  </div>
   );
 };
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+export default CommentDetail;
--- second-app/src/index.js
+++ second-app/src/index.js
@@ -2,8 +2,48 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 
+// Components
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+
 const App = () => {
-  return <div className="ui container comments"></div>;
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo={faker.date.weekday()}
+          avatarImage={faker.image.avatar()}
+          commentText="Great post!"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo={faker.date.weekday()}
+          avatarImage={faker.image.avatar()}
+          commentText="Thanks for the info!"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo={faker.date.weekday()}
+          avatarImage={faker.image.avatar()}
+          commentText="Amazing!!"
+        />
+      </ApprovalCard>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
