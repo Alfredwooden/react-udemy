3cfc182d63a267f8bcb3211fa8da08bbc463cd95
Alfredwooden <coomcs@gmail.com>
2020-02-21 14:49:36 -0300

    Section 5: Class 55

--- third-app/src/index.js
+++ third-app/src/index.js
@@ -1,15 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => {
-      console.log(position);
-    },
-    err => console.log(err)
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return <div>Hi there!</div>;
-};
+    this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => console.log(err)
+    );
+  }
+
+  // React says that we have to define Render!!!
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
