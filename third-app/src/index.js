3ae3975b29abf11fd99fcdf0a429d33eb7921baf
Alfredwooden <coomcs@gmail.com>
2020-02-27 08:16:51 -0300

    Section 6: Class 64

--- third-app/src/index.js
+++ third-app/src/index.js
@@ -1,20 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
+    // THIS IS THE ONLY TIME we do direct assignment
+    // to this.state
     this.state = { lat: null };
+  }
 
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        // We called setState!
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => {
-        this.setState({ errorMessage: err.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
@@ -25,7 +27,8 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      // return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <div>Loading...</div>;
